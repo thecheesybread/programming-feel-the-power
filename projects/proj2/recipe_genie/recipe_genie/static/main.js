30f415fd9c1d4e33020b089d31318fe5dce8addb
Ben Sklaroff <bsklaroff@gmail.com>
2013-03-20 05:35:23 -0700

    add ability to add and remove ingredients

--- projects/proj2/recipe_genie/recipe_genie/static/main.js
+++ projects/proj2/recipe_genie/recipe_genie/static/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   var allIngredients = [];
+  var currentIngredients = [];
   $.getJSON('/static/all_ingredients.json', function(data) {
     allIngredients = data;
   });
@@ -8,5 +9,44 @@ $(document).ready(function() {
     source: function(query, process) {
       process(allIngredients);
     }
+  });
+
+  var populateRecipes = function() {
+    console.log('TODO: populateRecipes');
+  };
+
+  var addIngredient = function() {
+    var ingredient = $('#new_ingredient_field').val();
+    if (allIngredients.indexOf(ingredient) !== -1 &&
+        currentIngredients.indexOf(ingredient) === -1) {
+      currentIngredients.push(ingredient);
+      var ingredientSpan = '<span class="ingredient">' + ingredient +
+        '<button class="close">&times;</button></span>';
+      $('#ingredients_container').append(ingredientSpan);
+      $('#new_ingredient_field').val('');
+      populateRecipes();
+    }
+  };
+
+  $('#new_ingredient_field').on("change", function(e) {
+    if (!e.originalEvent) {
+      addIngredient();
+    }
+  });
+
+  $('#add_ingredient_form').submit(function(e) {
+    e.preventDefault();
+    addIngredient();
+  });
+
+  $('#ingredients_container').on('click', 'button', function(e) {
+    var ingredientSpan = $(e.currentTarget).parent();
+    var ingredient = ingredientSpan.text().slice(0, -1);
+    var idx = currentIngredients.indexOf(ingredient);
+    if (idx !== -1) {
+      currentIngredients.splice(idx, 1);
+    }
+    ingredientSpan.remove();
+    populateRecipes();
   });
 });
